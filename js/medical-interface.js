c47c8d50a2503e1885c2a2afc2fbab9d12d521aa
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-03 17:31:57 -0700

    start separating front and back end?

--- js/medical-interface.js
+++ js/medical-interface.js
@@ -1,24 +1,16 @@
 // This file will retrieve form input
 var Doctor = require('./../js/medical.js').doctorModule;
-//test code
+
 var getDoctors = require('./../js/medical.js').getDoctors;
 $(function() {
   $('#doctorSearch').submit(function(event) {
     event.preventDefault();
     $('.results').show();
     var medicalIssue = $('#medicalIssue').val();
-    // var location = $('#location').val().toLowerCase();
     getDoctors(medicalIssue);
   });
   $('ul').on('click', 'li', function() {
     $(this.children).toggle();
   });
-  // $(".doctor").last().click(function() {
-  //
-  //   console.log('you clicked');
-  // });
+
 });
-
-
-
-//end test code
--- js/medical.js
+++ js/medical.js
@@ -5,9 +5,8 @@ function Doctor(name, address, phone) {
   this.address = address;
   this.phone = phone;
 }
-exports.doctorModule = Doctor;
-// This file will make an API request and display results
-exports.getDoctors = function(medicalIssue) {
+
+Doctor.prototype.getDoctors = function() {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.606209%2C-122.332071%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       $('#doctor-list').empty();
@@ -24,27 +23,5 @@ exports.getDoctors = function(medicalIssue) {
    .fail(function(error){
       console.log("fail");
     });
-};
-
-
-
-
-
-
-
-// exports.getDoctors = function(medicalIssue) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-//   .then(function(result) {
-//      console.log(result);
-//    })
-//     $('#doctorList').empty();
-//     for (i = 0; i < result.meta.count; i++) {
-//       var profile = result.data[i].profile;
-//       var name = profile.first_name + " " + profile.last_name + " " + profile.title;
-//       $('#doctorList').append('<li>' + name + '</li>');
-//     }
-//   }),
-//   .fail(function(error){
-//      console.log("fail");
-//    });
-// };
+}
+exports.doctorModule = Doctor;
