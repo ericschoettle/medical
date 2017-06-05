7cb10177263eb5d0ca9bf4ca10d71bb22c2a84b9
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-05 03:14:31 -0700

    separate front and back end, but needs updated export module.

--- js/medical-interface.js
+++ js/medical-interface.js
@@ -1,6 +1,14 @@
 var DoctorList = require('./../js/medical.js').listModule;
 
-var displayList = function(medicalIssue) {
+var displayList = function(result) {
+  for (i = 0; i < result.meta.count; i++) {
+    var profile = result.data[i].profile;
+    var practice = result.data[i].practices[0];
+    var name = profile.first_name + " " + profile.last_name + " " + profile.title;
+    var phone = practice.phones[0].number;
+    var address = practice.visit_address.street + ", " + practice.visit_address.street2 + ", " + practice.visit_address.city;
+    console.log(name);
+  }
   $('#doctor-list').empty();
   doctor = new Doctor(name, address, phone);
   $('#doctor-list').append('<li class="doctor">' + doctor.name + '<ul class="contact"><li>Address: '+ doctor.address + '</li><li>Phone: ' + phone + '</li></ul></li>');
@@ -12,7 +20,7 @@ $(document).ready(function() {
     event.preventDefault();
     $('.results').show();
     var medicalIssue = $('#medicalIssue').val();
-    var myList = currentList.getDoctors(medicalIssue);
+    var myList = currentList.getDoctors(medicalIssue, displayList);
   });
   $('ul').on('click', 'li', function() {
     $(this.children).toggle();
--- js/medical.js
+++ js/medical.js
@@ -9,23 +9,12 @@ function Doctor(name, address, phone) {
 function DoctorList() {
 }
 
-DoctorList.prototype.getDoctors = function(medicalIssue) {
+DoctorList.prototype.getDoctors = function(medicalIssue, displayList) {
   console.log('Get the Doctor');
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.606209%2C-122.332071%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
-      console.log(result);
-      for (i = 0; i < result.meta.count; i++) {
-        var profile = result.data[i].profile;
-        var practice = result.data[i].practices[0];
-        var name = profile.first_name + " " + profile.last_name + " " + profile.title;
-        var phone = practice.phones[0].number;
-        var address = practice.visit_address.street + ", " + practice.visit_address.street2 + ", " + practice.visit_address.city;
-        console.log(name);
-      }
+      displayList(result);
 // ^^^where i left off....
 
-
-
-      displayList(medicalIssue);
     }).fail(function(error){
        $('#doctor-list').text('error');
      });
