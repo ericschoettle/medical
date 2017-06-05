5658a8c86a1dfdbc936da3bd3ebf0c2d9bdc9507
Ilene Gorski <ilene.gorski@gmail.com>
2017-06-04 22:44:20 -0700

    separate front and back end; does not display

--- js/medical-interface.js
+++ js/medical-interface.js
@@ -1,24 +1,20 @@
-// This file will retrieve form input
-var Doctor = require('./../js/medical.js').doctorModule;
-//test code
-var getDoctors = require('./../js/medical.js').getDoctors;
-$(function() {
+var DoctorList = require('./../js/medical.js').listModule;
+
+var displayList = function(medicalIssue) {
+  $('#doctor-list').empty();
+  doctor = new Doctor(name, address, phone);
+  $('#doctor-list').append('<li class="doctor">' + doctor.name + '<ul class="contact"><li>Address: '+ doctor.address + '</li><li>Phone: ' + phone + '</li></ul></li>');
+};
+
+$(document).ready(function() {
+  var currentList = new DoctorList();
   $('#doctorSearch').submit(function(event) {
     event.preventDefault();
     $('.results').show();
     var medicalIssue = $('#medicalIssue').val();
-    // var location = $('#location').val().toLowerCase();
-    getDoctors(medicalIssue);
+    var myList = currentList.getDoctors(medicalIssue);
   });
   $('ul').on('click', 'li', function() {
     $(this.children).toggle();
   });
-  // $(".doctor").last().click(function() {
-  //
-  //   console.log('you clicked');
-  // });
 });
-
-
-
-//end test code
--- js/medical.js
+++ js/medical.js
@@ -5,46 +5,30 @@ function Doctor(name, address, phone) {
   this.address = address;
   this.phone = phone;
 }
-exports.doctorModule = Doctor;
-// This file will make an API request and display results
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.606209%2C-122.332071%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-      $('#doctor-list').empty();
+
+function DoctorList() {
+}
+
+DoctorList.prototype.getDoctors = function(medicalIssue) {
+  console.log('Get the Doctor');
+    $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=47.606209%2C-122.332071%2C10&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
+      console.log(result);
       for (i = 0; i < result.meta.count; i++) {
         var profile = result.data[i].profile;
-        var practice = result.data[i].practices[0]
+        var practice = result.data[i].practices[0];
         var name = profile.first_name + " " + profile.last_name + " " + profile.title;
         var phone = practice.phones[0].number;
         var address = practice.visit_address.street + ", " + practice.visit_address.street2 + ", " + practice.visit_address.city;
-        doctor = new Doctor(name, address, phone);
-        $('#doctor-list').append('<li class="doctor">' + doctor.name + '<ul class="contact"><li>Address: '+ doctor.address + '</li><li>Phone: ' + phone + '</li></ul></li>');
+        console.log(name);
       }
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+// ^^^where i left off....
+
+
+
+      displayList(medicalIssue);
+    }).fail(function(error){
+       $('#doctor-list').text('error');
+     });
 };
 
-
-
-
-
-
-
-// exports.getDoctors = function(medicalIssue) {
-//   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-//   .then(function(result) {
-//      console.log(result);
-//    })
-//     $('#doctorList').empty();
-//     for (i = 0; i < result.meta.count; i++) {
-//       var profile = result.data[i].profile;
-//       var name = profile.first_name + " " + profile.last_name + " " + profile.title;
-//       $('#doctorList').append('<li>' + name + '</li>');
-//     }
-//   }),
-//   .fail(function(error){
-//      console.log("fail");
-//    });
-// };
+exports.listModule = DoctorList;
